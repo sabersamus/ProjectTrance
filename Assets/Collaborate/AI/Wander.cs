358f06c535455f2f4548dd1d8f2851e2195fdb4d
RadioSpace <radiospace83@gmail.com>
2019-02-23 11:54:15 -0800

    Bug and Spawner Plus Corrected terrain
    maybe some extra stuff outside of the collaborate folder that can be deleted.

---
+++ Assets/Collaborate/AI/Wander.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+using Random = UnityEngine.Random;
+
+
+/// <summary>
+/// wanderers around a point
+/// </summary>
+public class Wander : MonoBehaviour
+{
+
+    public float radius;
+
+
+    
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent is null) throw new Exception("NavMeshAgent is Null");
+        else if (!agent.isOnNavMesh) Debug.Log("Agent is not on navmesh");
+
+
+        agent.SetDestination(GetRandomLocation());
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, agent.destination) < 1f)
+        {
+            Vector3 loc = GetRandomLocation();
+           
+            agent.SetDestination(loc);
+        }
+    }
+
+    Vector3 GetRandomLocation()
+    {
+
+        NavMeshHit hit;
+        
+        if (NavMesh.SamplePosition(transform.position + (Random.insideUnitSphere * radius), out hit, radius, 1 << NavMesh.GetAreaFromName("Walkable")))            
+        {
+
+           
+
+            
+
+           
+            return hit.position;
+        }       
+
+        //fuck!
+        return Vector3.zero;
+
+
+    }
+
+    void OnGUI()
+    {
+#if UNITY_EDITOR
+
+
+#endif
+    }
+}
+
