a1d5fc16e0aa839c2d1792745e696dac6634e458
Sabersamus <rcatron10@gmail.com>
2019-02-24 02:20:08 -0800

    Added ItemType
    ItemManager now has getGameItemByType(ItemType)

---
+++ Assets/Scripts/Inventory/ItemType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public enum ItemType : int
+{
+    //ORDER IS IMPORTANT
+    /// <summary>
+    /// ORDER IS IMPORTANT
+    /// MAYBE
+    /// </summary>
+    ///ORDER AND NAME IS IMPORTANT
+    WOOD,
+    STONE,
+    HEMP_FIBERS,
+    SULFUR,
+    CHARCOAL,
+    CLOTH,
+    GUNPOWDER
+
+
+}
