de00903203ce209dc9059b1a49d5f76892482dc2
Sabersamus <rcatron10@gmail.com>
2019-02-24 01:42:56 -0800

    Added ConsumableItem

---
+++ Assets/Scripts/Inventory/Items/ConsumableItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName ="ConsumableItem", menuName="GameItem/ConsumableItem", order =2)]
+public class ConsumableItem : GameItem
+{
+    [Serializable]
+    public enum ConsumableType
+    {
+        FOOD,
+        WATER,
+        MEDICAL
+    }
+
+    [SerializeField]
+    public ConsumableType consumableType;
+    [SerializeField]
+    public ConsumableEffect[] effects;
+
+}
