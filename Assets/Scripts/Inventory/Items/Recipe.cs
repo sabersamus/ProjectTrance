0d07b7ee0a368d779ee93d2f0620892e6c253eb6
Sabersamus <rcatron10@gmail.com>
2019-02-24 01:24:46 -0800

    Added CraftableItem
    Gunpowder being the first one

---
+++ Assets/Scripts/Inventory/Items/Recipe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Recipe", menuName = "Recipe/Recipe", order = 2)]
+public class Recipe : ScriptableObject
+{
+    [SerializeField]
+    public GameItem[] requiredItems;
+    [SerializeField]
+    public int[] requiredAmount;
+    [SerializeField]
+    public CraftableItem outputItem;
+    [SerializeField]
+    public int outputAmount;
+
+    public Dictionary<GameItem, int> input
+    {
+        get
+        {
+            Dictionary<GameItem, int> list = new Dictionary<GameItem, int>();
+            for (int i = 0; i < requiredItems.Length; i++)
+            {
+                list.Add(requiredItems[i], requiredAmount[i]);
+            }
+            return list;
+        }
+    }
+
+    public ItemStack itemStack
+    {
+        get
+        {
+            return new ItemStack(outputItem, outputAmount);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Inventory/RecipeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeManager : MonoBehaviour
+{
+    private static Dictionary<CraftableItem, Recipe> recipes;
+
+    void Awake()
+    {
+        recipes = new Dictionary<CraftableItem, Recipe>();
+        foreach (Recipe recipe in Resources.FindObjectsOfTypeAll<Recipe>() as Recipe[])
+        {
+            recipes.Add(recipe.outputItem, recipe);
+        }
+    }
+
+    public static Recipe getRecipeByItem(CraftableItem item)
+    {
+        return recipes[item];
+    }
+}
